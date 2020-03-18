22339265734b802d55e73307c3c0c19a24cad6e5
FTCIrrationalDoorkeepers4653 <55328090+FTCIrrationalDoorkeepers4653@users.noreply.github.com>
2020-03-18 13:04:58 -0400

    Add files via upload

---
+++ TeamCode-Files/CustomVuforia_Test.java
@@ -0,0 +1,126 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Extra;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Autonomous.Detectors.VuforiaImageInit;
+import org.firstinspires.ftc.teamcode.Autonomous.IDKAutonomousRobot;
+
+@Autonomous(name="Custom_Vuforia_TEST")
+//@Disabled
+public class CustomVuforia_Test extends LinearOpMode {
+    //Objects:
+    IDKAutonomousRobot autoRobot = new IDKAutonomousRobot("None");
+    VuforiaImageInit imageInit = new VuforiaImageInit();
+
+    //Variables (w/ Default Values):
+    boolean leftSide = false, rightSide = false;
+    int objectPosition = 2;
+
+    //Runtime Object:
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        autoRobot.init(hardwareMap);
+
+        waitForStart();
+        runtime.reset();
+
+        //Pipeline Runs (Blue Alliance, Reverse Values for Red Alliance):
+
+        /* Two Objects Pipeline:
+        leftSide = sampleLeftPipeline();
+        rightSide = sampleRightPipeline();
+
+        //Checking Case:
+        if (leftSide == true && rightSide == false) {
+          objectPosition = 1;
+        }
+
+        else if (leftSide == false && rightSide == true) {
+          objectPosition = 2;
+        }
+
+        else if (leftSide == false && rightSide == false) {
+          objectPosition = 3;
+        }
+
+        else {
+          objectPosition = 2; //Default Value
+        } */
+
+        //Three Object Detection Pipeline:
+        objectPosition = compareLightValues();
+
+        //Disables Vuforia:
+        imageInit.disableVuforia();
+
+        //Telemetry Data:
+        telemetry.addData("Classification: ", objectPosition);
+        telemetry.update();
+        sleep(10000);
+    }
+
+    //Sample Detection Pipelines (For Moto G5 Camera [1280x720 resized 128x72]):
+
+    //Two Object Detection Pipelines:
+    public boolean sampleLeftPipeline() {
+      int leftRGBArray[][] = imageInit.getRGBArray(0.1, 12, 20, 40, 32);
+      boolean foundLeft = imageInit.detectObject(leftRGBArray, 25, 10);
+
+      return foundLeft;
+    }
+
+    public boolean sampleRightPipeline() {
+      int rightRGBArray[][] = imageInit.getRGBArray(0.1, 76, 20, 40, 32);
+      boolean foundRight = imageInit.detectObject(rightRGBArray, 25, 10);
+
+      return foundRight;
+    }
+
+    //Three Object Detection Pipeline:
+    public int compareLightValues() {
+      int position = 2; //Default Value (1-Bridge, 2-Center, 3-Wall)
+
+      //Finds RGB Arrays for All Positions:
+      int leftRGBArray[][] =  imageInit.getRGBArray(0.1, 5, 25, 32, 22);
+      int centerRGBArray[][] = imageInit.getRGBArray(0.1, 47, 25, 32, 22);
+      int rightRGBArray[][] = imageInit.getRGBArray(0.1, 89, 25, 32, 22);
+
+      //Finds Average RGB Values (0-Darker -> 765-Lighter):
+      int leftStandardRGB[] = imageInit.getAverageRGBValues(leftRGBArray);
+      int centerStandardRGB[] = imageInit.getAverageRGBValues(centerRGBArray);
+      int rightStandardRGB[] = imageInit.getAverageRGBValues(rightRGBArray);
+
+      int leftRGB = leftStandardRGB[0] + leftStandardRGB[1] + leftStandardRGB[2];
+      int centerRGB = centerStandardRGB[0] + centerStandardRGB[1] + centerStandardRGB[2];
+      int rightRGB = rightStandardRGB[0] + rightStandardRGB[1] + rightStandardRGB[2];
+
+      //Checks for the Darkest Value:
+      if (leftRGB < rightRGB && leftRGB < centerRGB) {
+        position = 1;
+      }
+
+      else if (centerRGB < leftRGB && centerRGB < rightRGB) {
+        position = 2;
+      }
+
+      else if (rightRGB < leftRGB && rightRGB < centerRGB) {
+        position = 3;
+      }
+
+      else {
+        position = 2;
+      }
+
+      //Returns Position:
+      return position;
+    }
+}
+
---
+++ TeamCode-Files/VuforiaImageInit.java
@@ -0,0 +1,196 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Detectors;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Hardware;
+import com.vuforia.Image;
+import com.vuforia.PIXEL_FORMAT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
+import lib.Analyze;
+import lib.Executable;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+
+public class VuforiaImageInit extends Analyze {
+  //Hardware Map Object:
+  HardwareMap hardwareMap;
+
+  /* VUFORIA PIECES!!! */
+
+    //Custom Detectors:
+    public static Executable exe = new Executable();
+
+    //Vuforia Elements:
+    VuforiaLocalizer vuforia;
+    private static final String VUFORIA_KEY =
+            "AR7KPuz/////AAABmSKvAg58mkBSqvvfxvaYqxMN8S2CvbOIzcpLyLVqb9hLPXQf3hPCERtF9azaj5sBUezFRBqdVA53ZAsNmlWW/ThqkaHtmpKNqXneP6p8VhN4liG3ofA7Cidx234PKNIhalLvby0jdmuxT5Uhh4dJjST6taoZGArAQz7Df8hzPG26Nd92L1ATW3mO4qzNAny2UK5YrzG92bUIxqvpDLkjeq8UNTLHYD4ulI1i+Jl/dPzU2PdeNPEqlsykdshGvcuRWRz8qeMXfpKVZ9TXmLxqvuTe6K291gxuKtfWXJ11rYJHTJlUAvooMpPaAh2/isv6LUy83+3UhIyl1kNxaNeMHK52iqEjpswOiOmVkniWTblp";
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+
+  /* Constructor */
+  public VuforiaImageInit() {
+    super();
+  }
+
+  /* METHODS!!! */
+
+  //Initialize Vuforia:
+  public void initVuforia(HardwareMap hwMap, int[] detectorRGBArray, String detectorName) {
+      //PARAMS: MUST BE HARDWARE MAP OBJECT, RGB ARRAY OF 3, STRING NAME FOR DETECTOR
+
+      //Declares Hardware Map:
+      hardwareMap = hwMap;
+
+      /* Initaites Vuforia: */
+      int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+      VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+      //Sets the Vuforia Elements:
+      parameters.vuforiaLicenseKey = VUFORIA_KEY;
+      parameters.cameraDirection   = CAMERA_CHOICE;
+
+      //Instantiates the Vuforia engine:
+      vuforia = ClassFactory.getInstance().createVuforia(parameters);
+      Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
+
+      //Turns on the Flash for Detection:
+      com.vuforia.CameraDevice.getInstance().setFlashTorchMode(true);
+
+      /* INITS the Custom Detector */
+      try {
+          setDetectorRGB(detectorRGBArray[0], detectorRGBArray[1], detectorRGBArray[2]);
+          setDetectorName(detectorName);
+      }
+
+      catch (Exception e) {
+          e.printStackTrace();
+      }
+  }
+
+  //Disables Vuforia After Tracking:
+  public void  disableVuforia() {
+    //Turns off the Flash:
+    com.vuforia.CameraDevice.getInstance().setFlashTorchMode(false);
+
+    //Deactivates Vuforia Engine:
+    vuforia = null;
+  }
+
+  //RGB Comparison Method:
+  public int[] getAverageRGBValues(int singleRGBValues[][]) {
+    //Main Array:
+    int localAverage[] = new int[3];
+
+    try {
+      //Computes the Average:
+      localAverage = averageRGBValues(singleRGBValues);
+    }
+
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    //Returns RGB Average:
+    return localAverage;
+  }
+
+  //Detection Method:
+  public boolean detectObject(int[][] rgbValues, int lightingMargin, int pixelCount) {
+    //Main Boolean:
+    boolean mainBool = false; //Default Value
+
+    try {
+      //Finds the Value Number of Pixels and Sets Boolean:
+      int binaryValues[][] = boxDetector(rgbValues, lightingMargin);
+      mainBool = getBodyBoolean(binaryValues, pixelCount);
+    }
+
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    //Returns the Found Boolean:
+    return mainBool;
+  }
+
+  //Vuforia Capture Image Method:
+    public int[][] getRGBArray(Double resizedRatio, int startX, int startY, int width, int height) {
+      //PARAMS: RESIZED RATIO CANNOT BE ZERO, THE STARTX AND STARTY + THE WIDTH AND HEIGHT MUST BE LESS THAN OR EQUAL TO IMAGE WIDTH AND HEIGHT!!!
+        Image rgbImage;
+        Bitmap bitmapImage;
+        int rgbValues[][] = new int[width][height];
+
+        //Converts Frame to BitMa of RGB Format:
+        try {
+            VuforiaLocalizer.CloseableFrame frame = vuforia.getFrameQueue().take();
+            long numImages = frame.getNumImages();
+
+            formatLoop: for (int i = 0; i < numImages; i++) {
+                if (frame.getImage(i).getFormat() == PIXEL_FORMAT.RGB565) {
+                    //Creates Bitmap Image:
+                    rgbImage = frame.getImage(i);
+                    bitmapImage = Bitmap.createBitmap(rgbImage.getWidth(), rgbImage.getHeight(), Bitmap.Config.RGB_565);
+                    bitmapImage.copyPixelsFromBuffer(rgbImage.getPixels());
+
+                    //Gets Values for Bitmap Resizing:
+                    double currentWidth = bitmapImage.getWidth();
+                    double currentHeight = bitmapImage.getHeight();
+
+                    int newWidth = (int)(currentWidth*resizedRatio);
+                    int newHeight = (int)(currentHeight*resizedRatio);
+
+                    //Resizing Bitmap:
+                    bitmapImage = getResizedBitmap(bitmapImage, newWidth, newHeight);
+
+                    //Gets RGB Values Array from Bitmap:
+                    int turnsWidth = startX;
+
+                    while (turnsWidth < width+startX) {
+                        int turnsHeight = startY;
+                        while (startY < height+startY) {
+                            rgbValues[turnsWidth-startX][turnsHeight-startY] = bitmapImage.getPixel(turnsWidth, turnsHeight);
+
+                            turnsHeight++;
+                        }
+
+                        turnsWidth++;
+                    }
+
+                    //Breaks the for loop:
+                    break formatLoop;
+                }
+            }
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Returns the Obtained RGB Array:
+        return rgbValues;
+    }
+
+    //Resizing the Bitmap:
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        //Resize Variables:
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+}
